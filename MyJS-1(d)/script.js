d50ab24dd05ee014505c2e8a420e87482d7e767d
sss <matusoyo2436@gmail.com>
2024-09-08 18:42:48 +0900

    dditional text
    // price = 151;
    // price = price + 1;
    // price += 1;
    // price *= 1.5;
    price++;
    console.log(price * 120 * rate);
    console.log(price * 130 * rate);
    console.log(price * 140 * rate);

--- MyJS-1(d)/script.js
+++ MyJS-1(d)/script.js
@@ -57,4 +57,11 @@ console.log(price * 120 * rate);
 console.log(price * 130 * rate);
 console.log(price * 140 * rate);
 
-
+// price = 151;
+// price = price + 1;
+// price += 1;
+// price *= 1.5;
+price++;
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
