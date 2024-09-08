c2957387dfb0b05df46502bdc1d97ca8b43a329e
sss <matusoyo2436@gmail.com>
2024-09-08 15:56:40 +0900

    additional text
    console.log(100);
    console.log(-10);
    console.log(2.5);
    
    console.log(1.2e4);  // 1.2に10の4条 = 12000
    console.log(1.2e-4);  // 1.2に10の-4条 = 0.00012
    
    /*
    コメント
    */

--- MyJS-1(d)/script.js
+++ MyJS-1(d)/script.js
@@ -1,3 +1,12 @@
 alert("Hello World");
 
 console.log(100);
+console.log(-10);
+console.log(2.5);
+
+console.log(1.2e4);  // 1.2に10の4条 = 12000
+console.log(1.2e-4);  // 1.2に10の-4条 = 0.00012
+
+/*
+コメント
+*/
