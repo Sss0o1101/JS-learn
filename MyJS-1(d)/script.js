0158a77598437f56185e3633b35f85289813ab89
sss <matusoyo2436@gmail.com>
2024-09-08 16:07:28 +0900

    additional text
    コメント1
    */
    
    console.log(10 + 3);
    console.log(10 - 3); // 7
    console.log(10 * 3); // 30
    console.log(10 ** 3); // 1000
    console.log(10 / 3); // 3.3333....
    console.log(10 % 3); // 1
    console.log(10 + 2 * 3); // 16
    console.log((10 + 2) * 3); // 36
     No newline at end of file

--- MyJS-1(d)/script.js
+++ MyJS-1(d)/script.js
@@ -8,5 +8,14 @@ console.log(1.2e4);  // 1.2に10の4条 = 12000
 console.log(1.2e-4);  // 1.2に10の-4条 = 0.00012
 
 /*
-コメント
+コメント1
 */
+
+console.log(10 + 3);
+console.log(10 - 3); // 7
+console.log(10 * 3); // 30
+console.log(10 ** 3); // 1000
+console.log(10 / 3); // 3.3333....
+console.log(10 % 3); // 1
+console.log(10 + 2 * 3); // 16
+console.log((10 + 2) * 3); // 36
