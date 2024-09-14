e976dc4e4a89baf0b4d3f3ccbcf9cf9bb8aaa7f1
alias <matusoyo2436@gmail.com>
2024-09-14 13:19:59 +0900

    added text
    /*
    配列--------------------------------------------
    */
    
    const scores = [10, 20, 30, 40, 50];
    
    console.log(scores[0]);
    console.log(scores[1]);
    console.log(scores[2]);
    console.log(scores[3]);
    console.log(scores[4]);

---
+++ MyJS-2(d)/script.js
@@ -0,0 +1,11 @@
+/*
+配列--------------------------------------------
+*/
+
+const scores = [10, 20, 30, 40, 50];
+
+console.log(scores[0]);
+console.log(scores[1]);
+console.log(scores[2]);
+console.log(scores[3]);
+console.log(scores[4]);
