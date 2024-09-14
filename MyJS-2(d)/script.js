dad317b07137351e331da6c80d805f703a7fa004
alias <matusoyo2436@gmail.com>
2024-09-14 13:39:02 +0900

    added text
    console.log(scores.length); //5 (要素数)
    
    scores[1] = 11;  //値の変更
    console.log(scores); //[10, 11, 30, 40, 50]
    
    scores.push(60); //末尾に要素を追加
    scores.pop(); //末尾の要素を削除
    scores.unshift(70); //先頭に要素を追加
    scores.shift(); //先頭の要素を削除

--- MyJS-2(d)/script.js
+++ MyJS-2(d)/script.js
@@ -9,5 +9,12 @@ console.log(scores[1]); //20
 console.log(scores[2]); //30
 console.log(scores[3]); //40
 console.log(scores[4]); //50
+console.log(scores.length); //5 (要素数)
 
+scores[1] = 11;  //値の変更
+console.log(scores); //[10, 11, 30, 40, 50]
 
+scores.push(60); //末尾に要素を追加
+scores.pop(); //末尾の要素を削除
+scores.unshift(70); //先頭に要素を追加
+scores.shift(); //先頭の要素を削除
